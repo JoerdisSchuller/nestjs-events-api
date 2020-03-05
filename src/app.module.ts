1d7d540247a28943905f6db83e1327c534366fbd
Jördis Schuller <joerdis@Jordiss-MBP.fritz.box>
2020-03-05 19:56:01 +0100

    connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [EventsModule],
+  imports: [TypeOrmModule.forRoot(), EventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
